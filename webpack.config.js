6294548a4066da363c06207e1e8ece333e26a749
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-16 16:13:24 +0100

    Boiler plate set up for the beginning of the project.

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "WebpackBoiler",
+            title: "WeatherApp",
             filename: "index.html",
             template: "./src/template.html",
         }),
