2b2ed2107bb7b83bc6a862f19ae240b90bf57482
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-18 21:50:28 +0100

    added API and created get weather function

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ console.log("test");
 
 
 
-// const weatherAPI = '711063798872415aa7b221239241003'
-// const weatherLocation = 'Maastricht'
-
-// const  giphyAPI = 'ED9FuiTtRxtrUzJCFdwQ5IyzTYEJoZQY'
-
+const weatherAPI = "711063798872415aa7b221239241003";
+const weatherLocation = "Maastricht";
+const  giphyAPI = "ED9FuiTtRxtrUzJCFdwQ5IyzTYEJoZQY";
 
 
-// fetch(`https://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${weatherLocation}`)
-// .then((res)=> {
-//     res.json().then((data)=>{
-//         console.log(data.current.condition)
-//     })
-// })
-// .catch((err) => {
-//     console.log(err)
-// })
+async function getWeather(API,location){
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API}&q=${location}&days=7`);
+    const weatherData = await response.json();
+    return weatherData;
+ 
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    console.log(getWeather(weatherAPI, weatherLocation));
+});
+
+
