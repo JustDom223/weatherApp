b37cfb1f4c8541e52a7943ed4b85cf8b9c46e17b
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-18 21:26:04 +0100

    Added APIs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles/main.css";
 
 console.log("test");
+
+
+
+// const weatherAPI = '711063798872415aa7b221239241003'
+// const weatherLocation = 'Maastricht'
+
+// const  giphyAPI = 'ED9FuiTtRxtrUzJCFdwQ5IyzTYEJoZQY'
+
+
+
+// fetch(`https://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${weatherLocation}`)
+// .then((res)=> {
+//     res.json().then((data)=>{
+//         console.log(data.current.condition)
+//     })
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
