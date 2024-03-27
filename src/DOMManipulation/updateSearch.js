523a43c813ef40f9d7781305e365033d78e37d2f
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-27 22:42:29 +0100

    consolidated the sunrise and sunset Divs to be manipulated by a class instead of 2 ID's

--- src/DOMManipulation/updateSearch.js
+++ src/DOMManipulation/updateSearch.js
@@ -1,7 +1,6 @@
 export default function updateSearch(){
     const locationSearchBarElement = document.querySelector("#locationSearchBar");
     const newLocation = locationSearchBarElement.value;
-
     locationSearchBarElement.value = "";
     return newLocation;
 }
