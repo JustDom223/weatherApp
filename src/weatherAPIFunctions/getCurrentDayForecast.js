8ed69716a3c23ae7adae5ab1cae8ec2637a7b36c
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-27 16:34:52 +0100

    Created the function to extract the forecast info
    I think this has been made redundant now that I have a better understanding of accessing the info. I will create a better option for it soon

--- src/weatherAPIFunctions/getCurrentDayForecast.js
+++ src/weatherAPIFunctions/getCurrentDayForecast.js
@@ -1,3 +1,15 @@
-export default function getCurrentDayForecast(data){
-    data.current.
+export default async function getCurrentDayForecast(data){
+    try{
+        const forecastdays = await data.current;
+        const dayConditions = [];
+        forecastdays.forEach(day =>
+            dayConditions.push(day.day.condition.text));
+
+        return dayConditions;
+    }catch(err){
+        console.log(err);
+        return null;
+    }
 }
+
+function 
