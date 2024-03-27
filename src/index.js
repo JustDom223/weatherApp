251c3b563260814677334963072f87e424de9ec6
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-27 22:40:08 +0100

    added the additional functions for fleshing out the info board

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import displayCurrrentDayForecast from "./DOMManipulation/displayCurrentDayForecast";
+import displayCurrrentDayTimeline from "./DOMManipulation/displayCurrentDayTimeline";
 import displayDailyForecast from "./DOMManipulation/displayDailyForecast";
 import updateSearch from "./DOMManipulation/updateSearch";
 import "./styles/main.css";
-import getSevenDayConditionsArray from "./weatherAPIFunctions/getSevenDayConditionArray";
+import getForecastConditionsArray from "./weatherAPIFunctions/getForecastConditionsArray";
 import getWeather from "./weatherAPIFunctions/getWeather";
 
 const weatherAPI = "711063798872415aa7b221239241003";
-const weatherLocation = "Maastricht";
 // const  giphyAPI = "ED9FuiTtRxtrUzJCFdwQ5IyzTYEJoZQY";
 const numberOfDaysForecasted = 3;
 
@@ -18,9 +19,11 @@ document.addEventListener("DOMContentLoaded", async ()=> {
     updateButtonElement.addEventListener("click", async () => {
         const newSearch = updateSearch();
         const weatherData = await getWeather(weatherAPI, newSearch, numberOfDaysForecasted);
-        console.log(weatherData)
-        const conditionsArray = await getSevenDayConditionsArray(weatherData);
+        console.log(weatherData);
+        const conditionsArray = await getForecastConditionsArray(weatherData);
+        displayCurrrentDayForecast(weatherData);
         displayDailyForecast(conditionsArray);
+        displayCurrrentDayTimeline(weatherData);
 
     });
 });
