05da1ed891c0057bba824c5709337acaeceb9418
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-03-30 21:22:32 +0100

    Removed the redundant function and merged it into the DOM manipulation for the daily forecast

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import displayCurrentDayAdditionalInfo from "./DOMManipulation/displayCurrentDayAdditionalInfo";
 import displayCurrrentDayForecast from "./DOMManipulation/displayCurrentDayForecast";
 import displayCurrrentDayTimeline from "./DOMManipulation/displayCurrentDayTimeline";
 import displayDailyForecast from "./DOMManipulation/displayDailyForecast";
 import updateSearch from "./DOMManipulation/updateSearch";
 import "./styles/main.css";
-import getForecastConditionsArray from "./weatherAPIFunctions/getForecastConditionsArray";
 import getWeather from "./weatherAPIFunctions/getWeather";
 
 const weatherAPI = "711063798872415aa7b221239241003";
@@ -20,10 +20,10 @@ document.addEventListener("DOMContentLoaded", async ()=> {
         const newSearch = updateSearch();
         const weatherData = await getWeather(weatherAPI, newSearch, numberOfDaysForecasted);
         console.log(weatherData);
-        const conditionsArray = await getForecastConditionsArray(weatherData);
         displayCurrrentDayForecast(weatherData);
-        displayDailyForecast(conditionsArray);
+        displayDailyForecast(weatherData);
         displayCurrrentDayTimeline(weatherData);
+        displayCurrentDayAdditionalInfo(weatherData);
 
     });
 });
