cfd6866e881d5a2e47d8b23d3d15457bb30f8cf0
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-04-01 23:11:35 +0200

    Creating a switch function
    This function is designed to consolidate the types of weather based on what WeatherAPI supplies.
     as I am unable to get a different SVG img for every single weather type.
     It is then meant to return the desired image but it is currently not working.

--- src/DOMManipulation/displayCurrentDayForecast.js
+++ src/DOMManipulation/displayCurrentDayForecast.js
@@ -1,6 +1,84 @@
+// Import Functions
+import createSVGImg from "../imgs/svgs/imgManipulation/createSVGImg";
+// Import SVGS
+import sunriseSVG from "../imgs/svgs/sunriseSunset/reshot-icon-sun-arrow-96SYUPFDKA.svg";
+import sunsetSVG from "../imgs/svgs/sunriseSunset/reshot-icon-sun-arrow-EL79YXU2AJ.svg";
+
+import sunnySVG from "../imgs/svgs/weather/reshot-icon-sun-S9ZW4T6UGQ.svg";
+
+async function displayWeatherSVG(data){
+    try{
+        const currentWeather = await data.current.condition.text;
+        console.log(currentWeather);
+        switch (currentWeather) {
+        case "Clear":
+        case "Sunny":
+            console.log("The sun is out boiii");
+            break;
+        
+        case "Partly cloudy":
+        case "Cloudy":
+        case "Overcast":
+            console.log("Weather condition is cloudy.");
+            return createSVGImg(sunnySVG, "weatherSVG", "currentWeatherSVG");
+
+        case "Patchy light drizzle":
+        case "Light drizzle":
+        case "Moderate drizzle":
+        case "Heavy drizzle":
+        case "Patchy light rain":
+        case "Light rain":
+        case "Moderate rain":
+        case "Heavy rain":
+        case "Light freezing rain":
+        case "Moderate or heavy freezing rain":
+        case "Patchy light sleet":
+        case "Light sleet":
+        case "Moderate or heavy sleet":
+        case "Patchy light snow with thunder":
+        case "Moderate or heavy rain with thunder":
+            console.log("Weather condition is rainy.");
+            break;
+        
+        case "Patchy light snow":
+        case "Light snow":
+        case "Moderate snow":
+        case "Heavy snow":
+        case "Light sleet showers":
+        case "Moderate or heavy sleet showers":
+        case "Light snow showers":
+        case "Moderate or heavy snow showers":
+        case "Light showers of ice pellets":
+        case "Moderate or heavy showers of ice pellets":
+        case "Moderate or heavy snow with thunder":
+            console.log("Weather condition is snowy.");
+            break;
+      
+        case "Mist":
+        case "Fog":
+        case "Freezing fog":
+        case "Patchy fog":
+            console.log("Weather condition is foggy.");
+            break;
+      
+        default:
+            console.log("Weather condition is not categorized.");
+        }
+    }catch(err){
+        console.log(err);
+    }
+    
+}
+
+
 export default async function displayCurrrentDayForecast(data){
     const dayMainInfoElement = document.querySelector("#dayMainInfo");  
     dayMainInfoElement.textContent = "";
+    const sunriseSVGElement = createSVGImg(sunriseSVG, "sunSVG", "sunRiseSVG");
+    const sunsetSVGElement = createSVGImg(sunsetSVG, "sunSVG", "sunSetSVG");
+
+    const currentWeatherSVG = displayWeatherSVG(data);
+    console.log(currentWeatherSVG);
 
     const currentDayTempElement = document.createElement("p");
     const currentDayFeelsLikeElement = document.createElement("p");
@@ -8,12 +86,14 @@ export default async function displayCurrrentDayForecast(data){
     const locationElement = document.createElement("p");
     
     const daySunriseSunsetElement = document.createElement("div");
+    const daySunriseContainerElement= document.createElement("div");
+    const daySunsetContainerElement= document.createElement("div");
     const daySunriseElement = document.createElement("p");
     const daySunsetElement = document.createElement("p");
     // Adding classes and ID's to elements 
     daySunriseSunsetElement.id = "daySunriseSunset";
-    daySunriseElement.classList.add("sun");
-    daySunsetElement.classList.add("sun");
+    daySunriseContainerElement.classList.add("sun");
+    daySunsetContainerElement.classList.add("sun");
     // Adding element contents
     const tempC = true;
 
@@ -33,9 +113,17 @@ export default async function displayCurrrentDayForecast(data){
     daySunriseElement.textContent = `Sunrise: ${data.forecast.forecastday[0].astro.sunrise}`;
     daySunsetElement.textContent = `Sunset: ${data.forecast.forecastday[0].astro.sunset}`;
 
-    daySunriseSunsetElement.appendChild(daySunriseElement);
-    daySunriseSunsetElement.appendChild(daySunsetElement);
+    daySunriseContainerElement.appendChild(sunriseSVGElement);
+    daySunriseContainerElement.appendChild(daySunriseElement);
+    
+    daySunsetContainerElement.appendChild(sunsetSVGElement);
+    daySunsetContainerElement.appendChild(daySunsetElement);
 
+
+    daySunriseSunsetElement.appendChild(daySunriseContainerElement);
+    daySunriseSunsetElement.appendChild(daySunsetContainerElement);
+
+    // dayMainInfoElement.appendChild(currentWeatherSVG);
     dayMainInfoElement.appendChild(currentDayTempElement);
     dayMainInfoElement.appendChild(currentDayFeelsLikeElement);
     dayMainInfoElement.appendChild(tempRangeElement);
