3cf707795ace67aa3e6fa93e12f74818b376e55b
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-04-08 22:40:53 +0200

    created the error
    for when an invalid location is searched for

---
+++ src/DOMManipulation/locationError.js
@@ -0,0 +1,9 @@
+
+export default function locationErrorNotification(err){
+    const mainContentElement = document.createElement("div");
+
+    mainContentElement.id = "mainContent";
+    mainContentElement.textContent = 'Oops! Please use a valid location'
+    return mainContentElement;
+
+}
