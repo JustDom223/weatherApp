72a604de2fe985221645714a68bd94f1fcdbe7ea
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-04-09 22:01:01 +0200

    update

--- src/DOMManipulation/locationError.js
+++ src/DOMManipulation/locationError.js
@@ -3,7 +3,7 @@ export default function locationErrorNotification(err){
     const mainContentElement = document.createElement("div");
 
     mainContentElement.id = "mainContent";
-    mainContentElement.textContent = 'Oops! Please use a valid location'
+    mainContentElement.textContent = "Oops! Please use a valid location";
     return mainContentElement;
 
 }
